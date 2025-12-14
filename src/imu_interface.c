f39deb9928673c76f669178ec8587261daf7d069
AritzElge <aelguezabal010@gmail.com>
2025-12-14 22:58:34 +0100

    chore(style): Minor style issues solved. Keeping code consistent in style

--- src/imu_interface.c
+++ src/imu_interface.c
@@ -50,7 +50,8 @@ bool set_imu_gyrosFunction(get_imu_gyros fptr)
 	return ret_val;
 }
 
-bool set_imu_magFunction(get_imu_mag fptr){
+bool set_imu_magFunction(get_imu_mag fptr)
+{
 	bool ret_val = true;
 
     if (fptr == NULL)
@@ -69,27 +70,38 @@ bool set_imu_magFunction(get_imu_mag fptr){
 	return ret_val;
 }
 
-bool imu_config(imu_start imu_start_function){
+bool imu_config(imu_start imu_start_function)
+{
 	return imu_start_function();
 }
 
 bool get_imu_data(float* accelOX, float* accelOY, float* accelOZ,
 					float* gyrosOX, float* gyrosOY, float* gyrosOZ,
-					float* magOX, float* magOY, float* magOZ){
+					float* magOX, float* magOY, float* magOZ)
+{
 	bool ret_val = true;
-	if (stored_get_imu_accel != NULL){
+	if (stored_get_imu_accel != NULL)
+    {
 		ret_val = ret_val && stored_get_imu_accel(accelOX, accelOY, accelOZ);
-	} else {
+	}
+    else
+    {
 		ret_val = ret_val && false;
 	}
-	if (stored_get_imu_gyros != NULL) {
+	if (stored_get_imu_gyros != NULL)
+    {
 		ret_val = ret_val && stored_get_imu_gyros(gyrosOX, gyrosOY, gyrosOZ);
-	} else {
+	}
+    else
+    {
 		ret_val = ret_val && false;
 	}
-	if (stored_get_imu_mag != NULL){
+	if (stored_get_imu_mag != NULL)
+    {
 		ret_val = ret_val && stored_get_imu_mag(magOX, magOY, magOZ);
-	} else {
+	}
+    else
+    {
 		ret_val = ret_val && false;
 	}
 
